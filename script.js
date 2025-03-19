1785c43a2a0244839861a5b4981fede708c5c8dc
shubh-codes7 <shubhamcodes7@gmail.com>
2025-03-19 10:24:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let container = document.querySelector(".container")
+
+for(let i=0; i<800; i++){
+	container.innerHTML += `
+		<div class="square" ></div>
+	`
+}
+
+document.querySelectorAll(".square").forEach(square => {
+	square.addEventListener("mouseover", () => {
+		square.style.backgroundColor = getRandomColor();
+		setTimeout(()=>{
+			square.style.backgroundColor = "#494646"
+		}, 1000)
+	})
+})
+
+function getRandomColor() {
+        return `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+    }
